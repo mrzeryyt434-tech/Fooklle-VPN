bd24c5397f7f4cca0a40df41c0d96900756977e7
mrzeryyt434-tech <mrzeryyt434@gmail.com>
2026-01-17 14:13:40 +0300

    Update constants.dart

--- lib/core/model/constants.dart
+++ lib/core/model/constants.dart
@@ -1,13 +1,13 @@
 abstract class Constants {
-  static const appName = "Hiddify";
-  static const githubUrl = "https://github.com/hiddify/hiddify-next";
+  static const appName = "SneakyVPN";
+  static const githubUrl = "https://t.me/jakey_ru";
   static const githubReleasesApiUrl =
       "https://api.github.com/repos/hiddify/hiddify-next/releases";
   static const githubLatestReleaseUrl =
       "https://github.com/hiddify/hiddify-next/releases/latest";
   static const appCastUrl =
       "https://raw.githubusercontent.com/hiddify/hiddify-next/main/appcast.xml";
-  static const telegramChannelUrl = "https://t.me/hiddify";
+  static const telegramChannelUrl = "https://t.me/Sneaky_VPN";
   static const privacyPolicyUrl = "https://hiddify.com/privacy-policy/";
   static const termsAndConditionsUrl = "https://hiddify.com/terms/";
   static const cfWarpPrivacyPolicy =
